249cabf12768aa12da5f4b4b55f842bf48baa101
Avalcx <82713691@qq.com>
2022-03-10 11:01:22 +0800

    update

--- gps.py
+++ gps.py
@@ -4,26 +4,14 @@ import json
 import requests 
 import math
 
-
 pi = 3.141592653589793234
-r_pi = pi * 3000.0 / 180.0
-la = 6378245.0
-ob = 0.00669342162296594323
 
-
-def wgs84_gcj02(lng_wgs84, lat_wgs84):
+def lng_lat_algorithm(gps_lng, gps_lat):
 
     def lng_lat_convert(lng_lat_data):
         lng_lat_data = float(lng_lat_data) / 100.0
         lng_lat_data = int(lng_lat_data) + (lng_lat_data - int(lng_lat_data)) * 100.0 / 60.0 
-        return lng_lat_data
-    
-    def judge_China(lng, lat):
-        if lng < 70 or lng > 140:
-            return True
-        if lat < 0 or lat > 55:
-            return True      
-        return False
+        return lng_lat_data  
 
     def transformlat(lng, lat):
         r = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
@@ -39,34 +27,36 @@ def wgs84_gcj02(lng_wgs84, lat_wgs84):
         r += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 * math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
         return r
 
-    lng_wgs84 = lng_lat_convert(lng_wgs84)
-    lat_wgs84 = lng_lat_convert(lat_wgs84)
-
-    if judge_China(lng_wgs84, lat_wgs84):
-        return [lng_wgs84, lat_wgs84]
-
-    tlat = transformlat(lng_wgs84 - 105.0, lat_wgs84 - 35.0)
-    tlng = transformlng(lng_wgs84 - 105.0, lat_wgs84 - 35.0)
-    rlat = lat_wgs84 / 180.0 * pi
-    m = math.sin(rlat)
-    m = 1 - ob * m * m
-    sm = math.sqrt(m)
-    tlat = (tlat * 180.0) / ((la * (1 - ob)) / (m * sm) * pi)
-    tlng = (tlng * 180.0) / (la / sm * math.cos(rlat) * pi)
-    lat_gcj02 = lat_wgs84 + tlat
-    lng_gcj02 = lng_wgs84 + tlng
-    return [lng_gcj02, lat_gcj02]
+    def wgs84_gcj02(lng, lat):
+        lng = lng_lat_convert(lng)
+        lat = lng_lat_convert(lat)
+        rlat = lat / 180.0 * pi
+        m = 1 - 0.00669342162296594323 * math.sin(rlat) * math.sin(rlat)
+        sm = math.sqrt(m)
+        tlat = (transformlat(lng - 105.0, lat - 35.0) * 180.0) / ((6378245.0 * (1 - 0.00669342162296594323)) / (m * sm) * pi)
+        tlng = (transformlng(lng - 105.0, lat - 35.0) * 180.0) / (6378245.0 / sm * math.cos(rlat) * pi)
+        lng = lng + tlng
+        lat = lat + tlat
+        return [lng,lat]
 
 
-ser = serial.Serial("/dev/ttyUSB0", 9600 ) 
+    return wgs84_gcj02(gps_lng, gps_lat)
+
+ser = serial.Serial("/dev/ttyS0", 9600)
 while(True):
-        dataList = str(ser.readline())[2:][:-5].split(',')
-        if '$GNRMC' in dataList:
+        gpsList = str(ser.readline())[2:][:-3].split(',')
+        if '$GNRMC' in gpsList and gpsList[2] == "A":
+            try:
+               print(gpsList)
+               gcj02_lng = lng_lat_algorithm(gpsList[5],gpsList[3])[0]
+               gcj02_lat = lng_lat_algorithm(gpsList[5],gpsList[3])[1]
+               speed = str(round((float(gpsList[7]) * 1.852),2)) + "km/h"
+               url = "http://127.0.0.1:8080/api/status-info"
+               data = {"label":"gps","lng":gcj02_lng,"lat":gcj02_lat}
+               headers = {'content-type': 'application/json','charset': 'utf-8'}
+               res = requests.post(url,data=json.dumps(data),headers=headers)
+               data = {"label":"速度","value":speed,"color":"green"}
+               res = requests.post(url,data=json.dumps(data),headers=headers)
 
-           gcj02_lng = wgs84_gcj02(dataList[5],dataList[3])[0]
-           gcj02_lat = wgs84_gcj02(dataList[5],dataList[3])[1]
-  
-           url = "http://127.0.0.1:8080/api/status-info"
-           data = {"label":"gps","lat":gcj02_lat,"lng":gcj02_lng}
-           headers = {'content-type': 'application/json','charset': 'utf-8'}
-           res = requests.post(url,data=json.dumps(data),headers=headers)
+            except:
+               pass
